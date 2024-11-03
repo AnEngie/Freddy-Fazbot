da8cacb8eb50d836255ded0f688de22dd4f4c4ac
Sed <sed.van.hesselingen@hva.nl>
2024-11-04 00:59:56 +0100

    Added Command sparkle: Puts the sparkleOn meme ontop of a random pic + User can add which day they want

---
+++ src/commands/pic/sparkleOn.js
@@ -0,0 +1,60 @@
+const { AttachmentBuilder, ApplicationCommandOptionType, ApplicationCommand } = require('discord.js');
+const { createCanvas, loadImage } = require('@napi-rs/canvas');
+
+let lastImage = 0;
+let fontSize = 75;
+
+module.exports = {
+    name: 'sparkle',
+    description: 'don\'t forget to be yourself',
+    options: [
+        {
+            name: 'day',
+            description: 'day of the week',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+        }
+    ],
+    // devOnly: Boolean,
+    // testOnly: Boolean,
+    // deleted: true,
+
+    callback: async (client, interaction) => {
+        await interaction.deferReply();
+
+        const day = interaction.options.get('day');
+
+        let selectedImage = image_array[Math.floor(Math.random() * image_array.length)];
+
+        if (lastImage == selectedImage) {
+            selectedImage = image_array[Math.floor(Math.random() * image_array.length)];
+        }
+        lastImage = selectedImage;
+
+        const sparkleOnGif = await loadImage(`src/\\/sparkleOnGif/\\/sparkleOn.png`)
+
+        const guusImage = await loadImage(`src/\\/pictures/\\/${selectedImage}`)
+
+        const canvas = createCanvas(sparkleOnGif.width, sparkleOnGif.height);
+        const context = canvas.getContext('2d');
+
+        // This uses the canvas dimensions to stretch the image onto the entire canvas
+        context.drawImage(guusImage, 0, 0, canvas.width, canvas.height);
+
+        context.drawImage(sparkleOnGif, 0, 0, canvas.width, canvas.height)
+
+        do {
+            // Assign the font to the context and decrement it so it can be measured again
+            context.font = `${fontSize -= 10}px Script MT Bold`;
+            // Compare pixel width of the text to the canvas minus the approximate avatar size
+        } while (context.measureText(day.value).width > canvas.width - 300);
+
+        context.fillStyle = '#eb02b4'
+        context.fillText(day.value, 225, 555)
+
+        // Use the helpful Attachment class structure to process the file for you
+        const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'sparkle-on.png' });
+
+        interaction.editReply({ files: [attachment] });
+    },
+};
