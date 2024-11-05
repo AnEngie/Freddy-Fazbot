9bf50a14e1d2c14a42482d15e8d2f1eb749a287c
Sed <sed.van.hesselingen@hva.nl>
2024-11-05 02:28:12 +0100

    Added to Command hor: Bot can now send a mp3 randomly

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,3 +1,8 @@
+const { AttachmentBuilder } = require('discord.js');
+const horSound = (`src/\\/sounds/\\/hor.mp3`)
+
+const hor = new AttachmentBuilder(horSound, { name: 'hor.mp3' });
+
 module.exports = {
     name: 'hor',
     description: 'Hor',
@@ -7,8 +12,16 @@ module.exports = {
     // deleted: true,
 
     callback: (client, interaction) => {
-        random = ["Hor", "Hor Hor", "Hor 🫵"]
-        interaction.reply(`${random[Math.floor(Math.random() * random.length)]}`)
         console.log(`Ping: ${client.ws.ping} ms`)
+
+        random = ["Hor", "Hor Hor", "Hor 🫵"]
+        let chosenReply = Math.floor(Math.random() * (random.length + 1))
+
+        if (chosenReply >= random.length) {
+            interaction.reply({ files: [hor] });
+        }
+        else {
+            interaction.reply(`${random[chosenReply]}`)
+        }
     },
 };
